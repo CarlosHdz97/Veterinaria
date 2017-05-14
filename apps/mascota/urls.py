fcc541f89b147d2c475575b6aaed83a8ea85d43b
CARLOS <josecarlos19979@hotmail.com>
2017-05-14 16:15:42 -0500

    barra de navegacion configurada para dispositivos moviles

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -5,6 +5,6 @@ from apps.mascota.views import MascotaCreate, MascotaList, MascotaUpdate, Mascot
 urlpatterns = [
     url(r'^create/$',login_required(MascotaCreate.as_view()), name='create_pet'),
     url(r'^read/$',login_required(MascotaList.as_view()), name='principal'),
-    url(r'^update/(?P<pk>\w+)/$',login_required(MascotaUpdate.as_view()), name='update_pet'),
-    url(r'^delete/(?P<pk>\w+)/$', login_required(MascotaDelete.as_view()), name='delete_pet'),
+    url(r'^update/(?P<pk>\d+)/$',login_required(MascotaUpdate.as_view()), name='update_pet'),
+    url(r'^delete/(?P<pk>\d+)/$', login_required(MascotaDelete.as_view()), name='delete_pet'),
 ]
